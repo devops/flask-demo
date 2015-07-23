8a510b446e4ae160542e494756d4916d8c3fbd98
devops <devops@163.com>
2015-07-23 16:08:21 +0800

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,13 +2,10 @@
 # -*- coding: UTF-8 -*-
 
 from flask import Flask
-from flask.ext.pymongo import PyMongo
 from flask.ext.login import LoginManager
 
 from config import config
 
-
-mongo = PyMongo()
 login_manager = LoginManager()
 
 def create_app(config_name):
@@ -17,14 +14,7 @@ def create_app(config_name):
 
     from  app.core.views import mod as core_blueprint
     app.register_blueprint(core_blueprint, url_prefix='/')
-    from app.users.views import mod as users_blueprint
-    app.register_blueprint(users_blueprint, url_prefix='/users')
-    from  app.deploy.views import mod as deploy_blueprint
-    app.register_blueprint(deploy_blueprint, url_prefix='/deploy')
-    from app.assets.views import mod as assets_blueprint
-    app.register_blueprint(assets_blueprint, url_prefix='/assets')
-
-    mongo.init_app(app)
+    
     login_manager.init_app(app)
 
 
